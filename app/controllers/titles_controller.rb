29aa44ae9b21a8cd8ea023e39c39a32a24dcf951
istb0 <yamayosoou@gmail.com>
2021-09-18 23:16:24 +0900

    Improve: title create ajax

--- app/controllers/titles_controller.rb
+++ app/controllers/titles_controller.rb
@@ -2,11 +2,7 @@ class TitlesController < ApplicationController
   def create
     @illustration = Illustration.find(params[:illustration_id])
     @title = @illustration.titles.build(title_params)
-    if @title.save
-      redirect_to illustration_path(@title.illustration), success: t('defaults.message.posted', item: @title.model_name.human)
-    else
-      redirect_to illustration_path(@title.illustration), danger: t('defaults.message.not_posted', item: @title.model_name.human)
-    end
+    @title.save
   end
 
   private
